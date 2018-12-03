928f1803d24660181ff294d1669809c819a0d5e4
Pedro Sanchez <psanchez@daitangroup.com>
2018-12-03 13:51:27 -0200

    initial commit

---
+++ src/main/java/com/symphony/mvnrelease/MvnReleaseApplication.java
@@ -0,0 +1,12 @@
+package com.symphony.mvnrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnReleaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/symphony/mvnrelease/MvnReleaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.symphony.mvnrelease;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvnReleaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
